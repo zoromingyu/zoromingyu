e3ed0a6745a847543ad3fc5cc1bcbcc2b9609ce5
Kristen Won <kjwon@usc.edu>
2024-08-01 19:37:43 -0700

    byf dni

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,56 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import {useState} from "react";
 
 
 const About = () => {
 
-    const [isOn, setIsOn] = useState(false);
-    const expandSong = () => setIsOn(!isOn);
+    const [isDBig, setIsDBig] = useState(false);
+    const [isKBig, setIsKBig] = useState(false);
+
+    const Byf = ({ isVisible }) => (
+        <AnimatePresence initial={false}>
+            { isVisible && (
+                <motion.div
+                    animate={{
+                        opacity: 1
+                    }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
+                    <div className="flex flex-row gap-x-1 text-xs">
+                        <div className="font-bold">byf</div>
+                        <div>i go ia when i'm in school</div>
+                    </div>
+                </motion.div>
+            )
+
+            }
+        </AnimatePresence>
+    )
+
+    const Dni = ({ isVisible }) => (
+        <AnimatePresence initial={false}>
+            { isVisible && (
+                <motion.div
+                    animate={{
+                        opacity: 1
+                    }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
+                    <div className="flex flex-row gap-x-1 text-xs">
+                        <div className="font-bold">dni</div>
+                        <div className="flex flex-col">
+                            <div>exo/svt antis, btspoppers,</div>
+                            <div>annoying exols, -17/30+</div>
+                        </div>
+                    </div>
+                </motion.div>
+            )
+
+            }
+        </AnimatePresence>
+    )
 
     return (
         <div className="flex justify-center items-center h-screen bg-white">
@@ -22,25 +65,34 @@ const About = () => {
                         kristen
                     </div>
                     <div className="text-xs font-medium">
-                        she/her 20 korean enfp
+                        she/her 20 enfp 🇰🇷🇺🇸
                     </div>
                     <div className="flex flex-row items-center gap-x-2">
                         <img src="https://i.pinimg.com/736x/7b/f5/29/7bf5297b1565febb2359f5cd950399c0.jpg" alt="mingyu" className="rounded-full w-6"/>
-                        <Link to="/"><div className="text-xs font-bold">kristenwonton</div></Link>
+                        <Link to="/carrd"><div className="text-xs font-bold">kristenwonton</div></Link>
                     </div>
 
                     <div className="flex flex-col gap-y-2">
-                        <motion.div className="flex flew-row gap-x-20" whileHover={{scale: 1.1}} data-isOn={isOn} onClick={expandSong}>
-                            <div className="flex flex-row gap-x-3">
-                                <img src="https://i1.sndcdn.com/artworks-8yoXjdWWTgPX-0-t500x500.jpg" alt="dust" className="w-10 h-10"/>
-                                <div className="flex flex-col">
-                                    <div className="text-sm">Dust</div>
-                                    <div className="text-xs">SEVENTEEN</div>
+                            <motion.div
+                                className="flex flew-row gap-x-20"
+                                whileHover={{scale: 1.1}}
+                                onClick={() => setIsDBig((isDBig) => !isDBig)}
+                            >
+                                <div className="flex flex-row gap-x-3">
+                                    <img src="https://i1.sndcdn.com/artworks-8yoXjdWWTgPX-0-t500x500.jpg" alt="dust" className="w-10 h-10"/>
+                                    <div className="flex flex-col">
+                                        <div className="text-sm">Dust</div>
+                                        <div className="text-xs">SEVENTEEN</div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div>...</div>
-                        </motion.div>
-                        <motion.div className="flex flew-row gap-x-20" whileHover={{scale: 1.2}}>
+                                <div>...</div>
+                            </motion.div>
+                            <Byf isVisible={isDBig} />
+                        <motion.div
+                            className="flex flew-row gap-x-20"
+                            whileHover={{scale: 1.1}}
+                            onClick={() => setIsKBig((isKBig) => !isKBig)}
+                        >
                             <div className="flex flex-row gap-x-3">
                                 <img src="https://upload.wikimedia.org/wikipedia/en/5/51/Henggarae_seventeen_artwork.png" alt="kidult" className="w-10 h-10"/>
                                 <div className="flex flex-col">
@@ -50,6 +102,7 @@ const About = () => {
                             </div>
                             <div>...</div>
                         </motion.div>
+                        <Dni isVisible={isKBig}/>
                     </div>
                 </div>
             </div>
